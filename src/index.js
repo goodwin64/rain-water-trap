642c743521c2346f518f4098dad93c5823a9b347
Max Donchenko <wereya2@yandex.ru>
2018-01-05 01:40:06 +0200

    initWithStone method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const CODE_STONE = 1;
+const CODE_WATER = 2;
+const CODE_EMPTY = 0;
+
 module.exports = function trapRainWater(elevationMap) {
     const matrix = initWithStone(elevationMap);
     fillWithWater(matrix);
@@ -5,7 +9,24 @@ module.exports = function trapRainWater(elevationMap) {
 };
 
 function initWithStone(elevationMap) {
-    return [];
+    const rowsCount = Math.max.apply(null, elevationMap);
+    const emptyMatrix = new Array(rowsCount)
+        .fill(0)
+        .map(_ => []);
+
+    elevationMap.forEach((columnHeight, columnIndex) => {
+        for (
+            let i = emptyMatrix.length, j = 0;
+            i >= 0;
+            i--, j++
+        ) {
+            if (j < columnHeight) {
+                emptyMatrix[i][columnIndex] = CODE_STONE;
+            } else {
+                emptyMatrix[i][columnIndex] = CODE_EMPTY;
+            }
+        }
+    });
 }
 
 function fillWithWater(matrix) {
