cf22398a907575a20ec220a84d62c5d49ca547ef
Max Donchenko <wereya2@yandex.ru>
2018-01-05 01:59:43 +0200

    check "water possibility" on left and right: done

--- src/index.js
+++ src/index.js
@@ -3,36 +3,67 @@ const CODE_WATER = 2;
 const CODE_EMPTY = 0;
 
 module.exports = function trapRainWater(elevationMap) {
-    const matrix = initWithStone(elevationMap);
+    const matrix = fillWithStone(elevationMap);
     fillWithWater(matrix);
     return countWaterCells(matrix);
 };
 
-function initWithStone(elevationMap) {
+function fillWithStone(elevationMap) {
     const rowsCount = Math.max.apply(null, elevationMap);
-    const emptyMatrix = new Array(rowsCount)
+    const matrixToFill = new Array(rowsCount)
         .fill(0)
         .map(_ => []);
 
     elevationMap.forEach((columnHeight, columnIndex) => {
         for (
-            let i = emptyMatrix.length, j = 0;
+            let i = matrixToFill.length - 1, j = 0;
             i >= 0;
             i--, j++
         ) {
             if (j < columnHeight) {
-                emptyMatrix[i][columnIndex] = CODE_STONE;
+                matrixToFill[i][columnIndex] = CODE_STONE;
             } else {
-                emptyMatrix[i][columnIndex] = CODE_EMPTY;
+                matrixToFill[i][columnIndex] = CODE_EMPTY;
             }
         }
     });
+
+    return matrixToFill;
 }
 
 function fillWithWater(matrix) {
-    return matrix;
+    return matrix.map((row, indexRow) => {
+        return row.map((cell, indexColumn) => {
+            if (isWaterPossibleHere(matrix, indexRow, indexColumn)) {
+                matrix[indexRow][indexColumn] = CODE_WATER;
+            }
+        });
+    });
+}
+
+function isWaterPossibleHere(matrix, indexRow, indexColumn) {
+    const firstRowSize = matrix[indexRow].length;
+    if (indexColumn === 0 || indexColumn === firstRowSize - 1) {
+        return false;
+    }
+
+    const isEmptyOnLeft = matrix[indexRow]
+        .slice(0, indexColumn)
+        .every(cell => cell === CODE_EMPTY);
+    const isEmptyOnRight = matrix[indexRow]
+        .slice(indexColumn + 1)
+        .every(cell => cell === CODE_EMPTY);
+    if (isEmptyOnLeft || isEmptyOnRight) {
+        return false;
+    }
+
+    return matrix[indexRow][indexColumn] === CODE_EMPTY;
 }
 
 function countWaterCells(matrix) {
-    return 0;
+    return []
+        .concat
+        .apply([], matrix)
+        .filter(cell => cell === CODE_WATER)
+        .length;
 }
